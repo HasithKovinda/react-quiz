2b401c7c41eee2affae3b01cb2a61f69bc6646a6
HasithKovinda <it20032906@my.sliit.lk>
2024-01-11 23:54:39 +0530

    add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import QuizProvider from './context/context.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import QuizProvider from "./context/context.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QuizProvider>
       <App />
+      <Analytics />
     </QuizProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
